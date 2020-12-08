de99fb0f54d2c91885f086b07158620a9a681441
voson <i@voson.me>
2020-12-08 12:03:19 +0800

    优化

--- register.go
+++ register.go
@@ -8,18 +8,24 @@ import (
 )
 
 type Register interface {
-	GetName() string
 	GetStart() uint16
 	GetNum() uint16
 }
 
 type Readable interface {
-	Decode(data []byte) interface{}
+	// 为了适应单个寄存器，两个字节代表2个参数的情况和
+	// 单个寄存器，每个比特代表不同参数的情况
+	// 所以使用了result去获取decode结果
+	Decode(data []byte, results map[string]interface{})
 }
 
 type Writable interface {
-	Verify(value interface{}) error
-	Encode(value interface{}) []byte
+	GetName() string
+	// 为了适应单个寄存器，两个字节代表2个参数的情况和
+	// 单个寄存器，每个比特代表不同参数的情况
+	// 所以使用map来作为输入值，让寄存器自行取用输入值
+	Verify(params map[string]interface{}) error
+	Encode(params map[string]interface{}) []byte
 }
 
 type ReadableRegister interface {
@@ -29,13 +35,31 @@ type ReadableRegister interface {
 
 type ReadableRegisters []ReadableRegister
 
+// 必须是连续的寄存器才能读取
+func (rs ReadableRegisters) Bytes(address uint8) []byte {
+	f := &RTUFrame{Address: address, Function: Read}
+	SetDataWithRegisterAndNumber(f, rs.GetStart(), rs.GetNum())
+	return f.Bytes()
+}
+
+func (rs ReadableRegisters) GetStart() uint16 {
+	return rs[0].GetStart()
+}
+
+func (rs ReadableRegisters) GetNum() uint16 {
+	var result uint16
+	for _, r := range rs {
+		result = result + r.GetNum()
+	}
+	return result
+}
+
 func (rs ReadableRegisters) Decode(data []byte) map[string]interface{} {
 	m := make(map[string]interface{})
 	for _, r := range rs {
-		name := r.GetName()
 		start := r.GetStart()
 		end := start + r.GetNum()*2
-		m[name] = r.Decode(data[start:end])
+		r.Decode(data[start:end], m)
 	}
 	return m
 }
@@ -47,10 +71,6 @@ type Float32RoRegister struct {
 	Parse func(data float32) interface{}
 }
 
-func (f *Float32RoRegister) GetName() string {
-	return f.Name
-}
-
 func (f *Float32RoRegister) GetStart() uint16 {
 	return f.Start
 }
@@ -59,13 +79,14 @@ func (f *Float32RoRegister) GetNum() uint16 {
 	return f.Num
 }
 
-func (f *Float32RoRegister) Decode(data []byte) interface{} {
+func (f *Float32RoRegister) Decode(data []byte, results map[string]interface{}) {
 	bits := binary.LittleEndian.Uint32(data)
 	f32 := math.Float32frombits(bits)
 	if f.Parse != nil {
-		return f.Parse(f32)
+		results[f.Name] = f.Parse(f32)
+	} else {
+		results[f.Name] = f32
 	}
-	return f32
 }
 
 type WritableRegister interface {
@@ -74,6 +95,17 @@ type WritableRegister interface {
 }
 
 type WritableRegisters []WritableRegister
+
+func (ws WritableRegisters) Encode(params map[string]interface{}) ([]byte, error) {
+	result := make([]byte, 0)
+	for _, w := range ws {
+		if err := w.Verify(params); err != nil {
+			return nil, err
+		}
+		result = append(result, w.Encode(params)...)
+	}
+	return result, nil
+}
 
 type ReadableAndWritableRegister interface {
 	Register
@@ -104,11 +136,12 @@ func (s *StringRwRegister) GetNum() uint16 {
 	return s.Num
 }
 
-func (s *StringRwRegister) Decode(data []byte) interface{} {
-	return s.Parse(data)
+func (s *StringRwRegister) Decode(data []byte, results map[string]interface{}) {
+	results[s.Name] = s.Parse(data)
 }
 
-func (s *StringRwRegister) Verify(value interface{}) error {
+func (s *StringRwRegister) Verify(params map[string]interface{}) error {
+	value := params[s.Name]
 	str, ok := value.(string)
 	if !ok {
 		return fmt.Errorf("参数类型错误，期望: string,实际：%v", reflect.TypeOf(value))
@@ -119,7 +152,8 @@ func (s *StringRwRegister) Verify(value interface{}) error {
 	return nil
 }
 
-func (s *StringRwRegister) Encode(value interface{}) []byte {
+func (s *StringRwRegister) Encode(params map[string]interface{}) []byte {
+	value := params[s.Name]
 	dst := make([]byte, s.Num*2)
 	s.Bytes(value.(string), dst)
 	return dst
@@ -145,19 +179,21 @@ func (u *Uint16RwRegister) GetNum() uint16 {
 	return 1
 }
 
-func (u *Uint16RwRegister) Decode(data []byte) interface{} {
+func (u *Uint16RwRegister) Decode(data []byte, results map[string]interface{}) {
 	var order binary.ByteOrder = binary.BigEndian
 	if u.Order != nil {
 		order = u.Order
 	}
 	ui16 := order.Uint16(data)
 	if u.Parse != nil {
-		return u.Parse(ui16)
+		results[u.Name] = u.Parse(ui16)
+	} else {
+		results[u.Name] = ui16
 	}
-	return ui16
 }
 
-func (u *Uint16RwRegister) Verify(value interface{}) error {
+func (u *Uint16RwRegister) Verify(params map[string]interface{}) error {
+	value := params[u.Name]
 	f64, ok := value.(float64)
 	if !ok {
 		return fmt.Errorf("参数类型错误，期望: float64,实际：%v", reflect.TypeOf(value))
@@ -172,7 +208,8 @@ func (u *Uint16RwRegister) Verify(value interface{}) error {
 	return nil
 }
 
-func (u *Uint16RwRegister) Encode(value interface{}) []byte {
+func (u *Uint16RwRegister) Encode(params map[string]interface{}) []byte {
+	value := params[u.Name]
 	b := make([]byte, 2)
 	var order binary.ByteOrder = binary.BigEndian
 	if u.Order != nil {
@@ -182,47 +219,72 @@ func (u *Uint16RwRegister) Encode(value interface{}) []byte {
 	return b
 }
 
-type ByteRwRegister struct {
+type Param struct {
 	Name     string
-	Start    uint16
-	Order    binary.ByteOrder
-	Parse    func(data []byte) byte
 	Validate func(value byte) error
 }
 
-func (b *ByteRwRegister) GetName() string {
+// 单个寄存器，两个字节代表两个参数
+// 注意：Params长度必须<=2
+type DoubleParamRwRegister struct {
+	Name   string
+	Start  uint16
+	Params []Param
+}
+
+func (b *DoubleParamRwRegister) GetName() string {
 	return b.Name
 }
 
-func (b *ByteRwRegister) GetStart() uint16 {
+func (b *DoubleParamRwRegister) GetStart() uint16 {
 	return b.Start
 }
 
-func (b *ByteRwRegister) GetNum() uint16 {
+func (b *DoubleParamRwRegister) GetNum() uint16 {
 	return 1
 }
 
-func (b *ByteRwRegister) Decode(data []byte) interface{} {
-	return b.Parse(data)
+func (b *DoubleParamRwRegister) Decode(data []byte, results map[string]interface{}) {
+	if len(b.Params) > 2 {
+		panic(fmt.Sprintf("DoubleParamRwRegister must have less than 2 params,actual: %v", len(b.Params)))
+	}
+	// 边界检查
+	_ = data[1]
+	for index, p := range b.Params {
+		results[p.Name] = data[index]
+	}
 }
 
-func (b *ByteRwRegister) Verify(value interface{}) error {
-	f64, ok := value.(float64)
-	if !ok {
-		return fmt.Errorf("参数类型错误，期望: float64,实际：%v", reflect.TypeOf(value))
+func (b *DoubleParamRwRegister) Verify(params map[string]interface{}) error {
+	if len(b.Params) > 2 {
+		panic("DoubleParamRwRegister must have two params")
 	}
-	// 1个字节的数值范围
-	if f64 < 0 || f64 > 255 {
-		return fmt.Errorf("参数范围越界，期望: 0～255,实际：%v", f64)
+	for _, p := range b.Params {
+		name := p.Name
+		value := params[name]
+
+		f64, ok := value.(float64)
+		if !ok {
+			return fmt.Errorf("参数 %v 类型错误，期望: float64,实际：%v", name, reflect.TypeOf(value))
+		}
+		// 1个字节的数值范围
+		if f64 < 0 || f64 > 255 {
+			return fmt.Errorf("参数 %v  范围越界，期望: 0～255,实际：%v", name, f64)
+		}
+		if p.Validate != nil {
+			return p.Validate(byte(value.(float64)))
+		}
 	}
-	if b.Validate != nil {
-		return b.Validate(byte(f64))
-	}
+
 	return nil
 }
 
-func (b *ByteRwRegister) Encode(value interface{}) []byte {
-	return []byte{byte(value.(float64))}
+func (b *DoubleParamRwRegister) Encode(params map[string]interface{}) []byte {
+	buf := make([]byte, 2)
+	for index, p := range b.Params {
+		buf[index] = byte(params[p.Name].(float64))
+	}
+	return buf
 }
 
 type Float32RwRegister struct {
@@ -244,16 +306,18 @@ func (f *Float32RwRegister) GetNum() uint16 {
 	return 2
 }
 
-func (f *Float32RwRegister) Decode(data []byte) interface{} {
+func (f *Float32RwRegister) Decode(data []byte, results map[string]interface{}) {
 	bits := binary.LittleEndian.Uint32(data)
 	f32 := math.Float32frombits(bits)
 	if f.Parse != nil {
-		return f.Parse(f32)
+		results[f.Name] = f.Parse(f32)
+	} else {
+		results[f.Name] = f32
 	}
-	return f32
 }
 
-func (f *Float32RwRegister) Verify(value interface{}) error {
+func (f *Float32RwRegister) Verify(params map[string]interface{}) error {
+	value := params[f.Name]
 	f64, ok := value.(float64)
 	if !ok {
 		return fmt.Errorf("参数类型错误，期望: float64,实际：%v", reflect.TypeOf(value))
@@ -264,7 +328,8 @@ func (f *Float32RwRegister) Verify(value interface{}) error {
 	return nil
 }
 
-func (f *Float32RwRegister) Encode(value interface{}) []byte {
+func (f *Float32RwRegister) Encode(params map[string]interface{}) []byte {
+	value := params[f.Name]
 	bits := math.Float32bits(float32(value.(float64)))
 
 	bytes := make([]byte, 4)
